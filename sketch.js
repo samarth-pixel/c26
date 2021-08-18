a4b0d54365d28bf32834fd82c98d975dd921cfe0
samarth-pixel <samarth.vastrad@gmail.com>
2021-08-17 20:30:21 -0400

    New log has been created

--- sketch.js
+++ sketch.js
@@ -6,6 +6,7 @@ var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
 
+
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -33,6 +34,7 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    log6 = new Log(300,500,120, PI/4);
 
     bird = new Bird(100,100);
 
@@ -58,7 +60,7 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
+    log6.display();
     bird.display();
     platform.display();
 }
